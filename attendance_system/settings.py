6790571c8c3b33920ea83b4a695717b45b46a6cd
Larry-007-del <175581182+Larry-007-del@users.noreply.github.com>
2026-02-15 18:54:28 +0000

    Switch to non-strict static files storage

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Fix for WhiteNoise - Ignore missing files in CSS (prevents Jazzmin error)
 WHITENOISE_MANIFEST_STRICT = False
